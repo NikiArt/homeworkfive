fb631d8c2b15edcc4b90b5d18413178636f31924
Nikita Boiko <genertyniki@gmail.com>
2019-10-22 19:00:49 +0300

    Initial Commit

---
+++ homeworkfive/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  homeworkfive
+//
+//  Created by Nikita Boiko on 22.10.2019.
+//  Copyright © 2019 Nikita Boiko. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
